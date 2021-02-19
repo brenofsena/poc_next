da506611996be228039454fc7f0131a6adc35588
Breno Sena <breno.fsena@gmail.com>
2021-02-18 23:35:42 -0300

    Initial commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,17 @@
+﻿import * as S from './styles'
+
+const Main = ({
+  title = 'Next Boilerplate',
+  description = 'ReactJS, TypeScript, NextJS and Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/images/next.svg"
+      alt="Uma desenvolvedora ao lado da palavra next.js"
+    />
+  </S.Wrapper>
+)
+
+export default Main
